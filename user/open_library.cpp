df80f1eb7c6914c0a1977a6cd40dd0b2740b5b45
puduk <demirmert986@gmail.com>
2025-05-07 23:15:20 +0200

    Bug fix: open_library.cpp

--- user/open_library.cpp
+++ user/open_library.cpp
@@ -1,6 +1,7 @@
 #include "open_library.hpp"
 #include <iostream>
 #include <fstream>
+#include <algorithm> // for std::replace
 #include "user_menu.hpp"
 
 void open_libraries(Data& u){
@@ -8,7 +9,7 @@ void open_libraries(Data& u){
   std::cout << "------------------------------" << '\n';
   std::cout << "Current avaliable libraries:" << '\n';
 
-  std::ifstream amount_file ("library.txt" , std::ios::in);
+  std::ifstream amount_file("library.txt", std::ios::in);
   if(!amount_file.is_open()){
     std::cout << "Currently no any avaliable library." << '\n';
     return;
@@ -20,10 +21,11 @@ void open_libraries(Data& u){
       (*u.library_amount)++;
     }
   }
+  amount_file.close();
 
-  std::ifstream show_library("library.txt" , std::ios::in);
-  if (!show_library.is_open()){
-    std::cout << "Current no any avaliable library." << '\n';
+  std::ifstream show_library("library.txt", std::ios::in);
+  if(!show_library.is_open()){
+    std::cout << "Currently no any avaliable library." << '\n';
     return;
   }
 
@@ -33,6 +35,8 @@ void open_libraries(Data& u){
       std::cout << line << '\n';
     }
   }
+  show_library.close();
+
   std::cout << "Which library you want to open?" << '\n';
   std::cin >> *u.library_choice;
 
@@ -41,31 +45,35 @@ void open_libraries(Data& u){
     return;
   }
   if(*u.library_choice > *u.library_amount ){
-    std::cout << "Opps! We dont have that." << '\n';
+    std::cout << "Oops! We don't have that." << '\n';
     return;
   }
 
-
-  std::ifstream choose_file ("library.txt" , std::ios::in);
+  std::ifstream choose_file("library.txt", std::ios::in);
   if(!choose_file.is_open()){
-    std::cout << "Current no any avaliable library." << '\n';
+    std::cout << "Currently no any avaliable library." << '\n';
     return;
   }
-
 
   std::string choose_line;
-
-
+  std::string key = std::to_string(*u.library_choice) + ". Library Name : ";
 
   while(std::getline(choose_file, choose_line)){
-    if(choose_line.find(std::to_string(*u.library_choice) + ". ") != std::string::npos){
-      *u.choosen_library_name = choose_line.substr(choose_line.find(":")+2);
+    if(choose_line.find(key) != std::string::npos){
+      std::string raw_name = choose_line.substr(key.length());
+
+      while (!raw_name.empty() && std::isspace(raw_name.front()))
+        raw_name.erase(raw_name.begin());
+      while (!raw_name.empty() && std::isspace(raw_name.back()))
+        raw_name.pop_back();
+
+      std::replace(raw_name.begin(), raw_name.end(), ' ', '_');
+
+      *u.choosen_library_name = raw_name;
+      break;
     }
   }
+  choose_file.close();
 
-
-
-  user_menu(u );
-
+  user_menu(u);
 }
-
