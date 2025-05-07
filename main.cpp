48c6558e885c4c3d07993ed3fa8735bb4659da7c
puduk <demirmert986@gmail.com>
2025-05-07 21:44:48 +0200

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "first/first_data.hpp"
+#include "first/first_page.hpp"
+#include "library/library_data.hpp"
+#include "user/data.hpp"
+
+int main() {
+
+    FirstData f;
+    Data u;
+    LibraryData l;
+
+    first_page(f , u ,l);
+
+
+
+}
