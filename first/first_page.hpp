dddb1834c89ecd37284f1607b1b0c90fe7b383ef
puduk <demirmert986@gmail.com>
2025-05-07 21:42:08 +0200

    Add first_page.hpp

---
+++ first/first_page.hpp
@@ -0,0 +1,11 @@
+#ifndef FIRST_PAGE_H
+#define FIRST_PAGE_H
+#include "first_data.hpp"
+#include "../library/library_data.hpp"
+#include "../user/data.hpp"
+
+
+
+void first_page(FirstData& f  , Data& u , LibraryData& l );
+
+#endif
