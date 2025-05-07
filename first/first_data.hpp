4c335943a252e522a3d3f4b2d243a8fbe4e7c51d
puduk <demirmert986@gmail.com>
2025-05-07 21:42:03 +0200

    Add first_data.hpp

---
+++ first/first_data.hpp
@@ -0,0 +1,21 @@
+#ifndef FIRST_DATA_H
+#define FIRST_DATA_H
+#include <iostream>
+
+class FirstData{
+  public:
+
+    int* first_page_choice ;
+    int* rate ;
+    char* who;
+
+    std::string* role;
+
+
+    FirstData();
+    ~FirstData();
+
+};
+
+#endif
+
