a761b862b0c13949a8172bada04d7b5e750af9cd
puduk <demirmert986@gmail.com>
2025-05-07 21:43:05 +0200

    Add show_deleted.cpp

---
+++ library/show_deleted.cpp
@@ -0,0 +1,28 @@
+#include "show_deleted.hpp"
+#include <iostream>
+#include <fstream>
+
+void show_deleted(LibraryData& l){
+
+  std::cout << "Deleted Subjects : " << '\n';
+
+  std::ifstream show_file(*l.library_name + "_removed.txt" , std::ios::in);
+  if(!show_file.is_open()){
+    std::cout << "Unable to open file." << '\n';
+    return;
+  }
+
+  show_file.seekg(0, std::ios::end);
+  if(show_file.tellg() == 0){
+    std::cout << "There is no deleted book." << '\n';
+    return;
+  }
+
+  show_file.seekg(0);
+
+  std::string line;
+  while(std::getline(show_file, line)){
+    std::cout << line << '\n';
+  }
+}
+
